7a4cf01027ad8bab92f175ac21410ce6c88837ac
Brandon Fraustro <brandon.fraustro@almamartech.com>
2024-08-12 16:21:01 -0600

    perf: improve menu responsive

--- src/slices/Menu/index.tsx
+++ src/slices/Menu/index.tsx
@@ -38,9 +38,7 @@ const Menu = ({ slice }: MenuProps): JSX.Element => {
                 <PrismicNextImage alt="" field={slice.primary.logo} />
               </Link>
             </div>
-            <div className="menu-slice__hamburger" onClick={toggleMenu}>
-              <Image src="/assets/images/icons8-menú.svg" alt={"icon"} width={30} height={30} />
-            </div>
+            
             <div className={`menu-slice__links-container ${isMenuOpen ? 'open' : ''}`}>
               <div className="menu-slice__list-links">
                 {slice.primary.groupmenu.map((item, index) => (
@@ -57,6 +55,12 @@ const Menu = ({ slice }: MenuProps): JSX.Element => {
                   <>{slice.primary.labelbutton}</>
                 </PrismicNextLink>
               </div>
+              <div className="menu-slice__icon" onClick={toggleMenu}>
+                <Image src="/assets/images/icons8-eliminar.svg" alt={"icon"} width={30} height={30} />
+              </div>
+            </div>
+            <div className="menu-slice__icon" onClick={toggleMenu}>
+              <Image src="/assets/images/icons8-menú.svg" alt={"icon"} width={30} height={30} />
             </div>
           </div>
         </header>
