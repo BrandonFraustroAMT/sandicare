aace172e885463134a3f7ef1bf2224fbf848f36b
Brandon Fraustro <brandon.fraustro@almamartech.com>
2024-08-28 14:46:41 -0600

    feat: test new iframe page

---
+++ src/app/agenda/page.tsx
@@ -0,0 +1,11 @@
+export default async function Page() {
+  return (
+    <main>
+      <div style={{position: "relative", width: "100%", height: "0", paddingTop: "56.2500%", paddingBottom: "0", boxShadow: "0 2px 8px 0 rgba(63,69,81,0.16)", marginTop: "1.6em", marginBottom: "0.9em", overflow: "hidden", borderRadius: "8px", willChange: "transform"}}>
+        <iframe loading="lazy" style={{position: "absolute", width: "100%", height: "100%", top: "0", left: "0", border: "none", padding: "0", margin: "0"}}src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGPLDtawxc&#x2F;8PMqXyXcW0tyIQsmv7qKMA&#x2F;view?embed" allowFullScreen={true} allow="fullscreen">
+        </iframe>
+        </div>
+        <a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGPLDtawxc&#x2F;8PMqXyXcW0tyIQsmv7qKMA&#x2F;view?utm_content=DAGPLDtawxc&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">Copia de SandiCare Pitch Checkatusalud</a> de Daniel Arias
+    </main>
+  );
+}
