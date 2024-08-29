213a6fa1c529bd309b99676fd81931511b283dbf
Brandon Fraustro <brandon.fraustro@almamartech.com>
2024-08-28 23:12:43 -0600

    perf: change iframe the rest of canvas page

--- src/app/telemedicinapt/page.tsx
+++ src/app/telemedicinapt/page.tsx
@@ -3,10 +3,17 @@ import '../conocenospt/agenda.css'
 export default async function Page() {
   return (
     <main className='main-container'>
-      <div className='agenda-container'>
+      {/* <div className='agenda-container'>
         <iframe loading="lazy" className='agenda-iframe' src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGPLXL_u_I&#x2F;t6MJoOiSwDSsFazpDNbEdw&#x2F;view?embed" allowFullScreen={true} allow="fullscreen">
         </iframe>
-        </div>
+      </div> */}
+      <div style={{position: "relative", width: "100%", height: "0", paddingTop: "46.25%;",
+       paddingBottom: "0", boxShadow: "0 2px 8px 0 rgba(63,69,81,0.16)", overflow: "hidden",
+       borderRadius: "8px", willChange: "transform"}}>
+        <iframe loading="lazy" style={{position: "absolute", width: "100%", height: "100%", top: "0", left: "0", border: "none", padding: "0",margin: "0"}}
+          src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGPLXL_u_I&#x2F;t6MJoOiSwDSsFazpDNbEdw&#x2F;view?embed" allowFullScreen={true} allow="fullscreen">
+        </iframe>
+      </div>
     </main>
   );
 }
