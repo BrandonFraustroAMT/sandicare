186078079b233f68269ead203065bd3dab8be6a6
Brandon Fraustro <brandon.fraustro@almamartech.com>
2024-08-29 22:48:24 -0600

    perf: add states to rest of pt pages

--- src/app/sandicheckpt/page.tsx
+++ src/app/sandicheckpt/page.tsx
@@ -1,13 +1,32 @@
+"use client"
+import { useEffect, useState } from 'react';
 import '../conocenospt/agenda.css'
 
 export default function Page() {
+  const [iframeLoaded, setIframeLoaded] = useState(false);
+
+  useEffect(() => {
+    const iframe = document.querySelector('iframe');
+    if (iframe) {
+      const handleLoad = () => setIframeLoaded(true);
+      iframe.addEventListener('load', handleLoad);
+
+      return () => iframe.removeEventListener('load', handleLoad); // Cleanup
+    }
+  }, []);
   return (
     <main className='main-container'>
-      <div style={{position: "relative", width: "100%", height: "0", paddingTop: "46.25%;",
-       paddingBottom: "0", boxShadow: "0 2px 8px 0 rgba(63,69,81,0.16)", overflow: "hidden",
-       borderRadius: "8px", willChange: "transform"}}>
-        <iframe loading="lazy" style={{position: "absolute", width: "100%", height: "100%", top: "0", left: "0", border: "none", padding: "0",margin: "0"}}
-          src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGPL5NMRqM&#x2F;8s7VLUERDqteS28Agpnp-g&#x2F;view?embed" allowFullScreen={true} allow="fullscreen">
+      <div style={{
+        position: "relative", width: "100%", height: "0", paddingTop: "46.25%",
+        boxShadow: "0 2px 8px 0 rgba(63,69,81,0.16)", overflow: "hidden",
+        borderRadius: "8px", willChange: "transform"
+      }}>
+        {!iframeLoaded && <p>Loading...</p>}
+        <iframe loading="lazy" style={{
+          position: "absolute", width: "100%", height: "100%", top: "0", left: "0",
+          border: "none"
+        }}
+          src="https://www.canva.com/design/;DAGPL5NMRqM/8s7VLUERDqteS28Agpnp-g/view?embed" allow="fullscreen">
         </iframe>
       </div>
     </main>
--- src/app/telemedicinapt/page.tsx
+++ src/app/telemedicinapt/page.tsx
@@ -1,17 +1,32 @@
+"use client"
+import { useEffect, useState } from 'react';
 import '../conocenospt/agenda.css'
 
 export default function Page() {
+  const [iframeLoaded, setIframeLoaded] = useState(false);
+
+  useEffect(() => {
+    const iframe = document.querySelector('iframe');
+    if (iframe) {
+      const handleLoad = () => setIframeLoaded(true);
+      iframe.addEventListener('load', handleLoad);
+
+      return () => iframe.removeEventListener('load', handleLoad); // Cleanup
+    }
+  }, []);
   return (
     <main className='main-container'>
-      {/* <div className='agenda-container'>
-        <iframe loading="lazy" className='agenda-iframe' src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGPLXL_u_I&#x2F;t6MJoOiSwDSsFazpDNbEdw&#x2F;view?embed" allowFullScreen={true} allow="fullscreen">
-        </iframe>
-      </div> */}
-      <div style={{position: "relative", width: "100%", height: "0", paddingTop: "46.25%;",
-       paddingBottom: "0", boxShadow: "0 2px 8px 0 rgba(63,69,81,0.16)", overflow: "hidden",
-       borderRadius: "8px", willChange: "transform"}}>
-        <iframe loading="lazy" style={{position: "absolute", width: "100%", height: "100%", top: "0", left: "0", border: "none", padding: "0",margin: "0"}}
-          src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGPLXL_u_I&#x2F;t6MJoOiSwDSsFazpDNbEdw&#x2F;view?embed" allowFullScreen={true} allow="fullscreen">
+      <div style={{
+        position: "relative", width: "100%", height: "0", paddingTop: "46.25%",
+        boxShadow: "0 2px 8px 0 rgba(63,69,81,0.16)", overflow: "hidden",
+        borderRadius: "8px", willChange: "transform"
+      }}>
+        {!iframeLoaded && <p>Loading...</p>}
+        <iframe loading="lazy" style={{
+          position: "absolute", width: "100%", height: "100%", top: "0", left: "0",
+          border: "none"
+        }}
+          src="https://www.canva.com/design/DAGPLXL_u_I/t6MJoOiSwDSsFazpDNbEdw/view?embed" allow="fullscreen">
         </iframe>
       </div>
     </main>
